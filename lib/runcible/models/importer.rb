ef27f8c9f7964eb71b1208038ab5cc9191d2a05a
David Davis <daviddavis@redhat.com>
2016-01-31 13:13:15 -0500

    Fixes #13452 - Support download_policy for importer

--- lib/runcible/models/importer.rb
+++ lib/runcible/models/importer.rb
@@ -6,13 +6,19 @@ module Runcible
     # Generic class to represent Pulp Importers
     # Child classes should supply id & config methods
     class Importer
+      DOWNLOAD_POLICIES = [
+        DOWNLOAD_IMMEDIATE = 'immediate',
+        DOWNLOAD_ON_DEMAND = 'on_demand',
+        DOWNLOAD_BACKGROUND = 'background'
+      ].freeze
+
       # https://github.com/pulp/pulp/blob/2.7-testing/common/pulp/common/plugins/importer_constants.py
       attr_accessor 'feed', 'validate',
                     'ssl_ca_cert', 'ssl_validation', 'ssl_client_cert', 'ssl_client_key',
                     'proxy_host', 'proxy_port', 'proxy_username', 'proxy_password',
                     'basic_auth_username', 'basic_auth_password',
                     'max_downloads', 'max_speed',
-                    'remove_missing', 'retain_old_count'
+                    'remove_missing', 'retain_old_count', 'download_policy'
 
       def initialize(params = {})
         params.each { |k, v| send("#{k}=", v) }
